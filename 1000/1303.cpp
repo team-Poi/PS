1fa32e784586ad6c715975fa4a331cbefdb9fa96
Oein <oein0219@gmail.com>
2022-06-08 16:29:29 +0900

    Added 1303.cpp

---
+++ 1000/1303.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n , m;
+    scanf("%d %d" , &n , &m);
+    for(int i = 0;i < n;i++){
+        for(int j = 1;j <= m;j++){
+            printf("%d " , i * m + j);
+        }
+        printf("\n");
+    }
+}
