c47361370e2ddfb0c72c213012069309e9ec1afd
Oein <oein0219@gmail.com>
2022-06-19 17:00:54 +0900

    1?? Pros

---
+++ 1__/103.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+int main(){
+	std::cout << "TTTTTTTTTT\nTTTTTTTTTT\n    TT\n    TT\n    TT";
+}
---
+++ 1__/104.cpp
@@ -0,0 +1,4 @@
+#include<iostream>
+int main(){
+	std::cout << "kor 90\nmat 80\neng 100\nsum 270";
+}
