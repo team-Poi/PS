720cd27402c7e2c415931265aa40e500b69cca07
Oein <oein0219@gmail.com>
2022-06-19 15:40:55 +0900

    Added 4698.cpp

---
+++ 4000/4698.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int , int> pi_i;
+
+int main(){
+    cin.tie(0); cout.tie(0);
+    char x[26];
+    for(int i = 0;i < 26;i++) x[i] = 'A' + i;
+    for(auto j : x) cout << j << " ";
+}
