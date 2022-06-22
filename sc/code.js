47ed9211cbec5932f521303e680e28596cdbc961
Oein <oein0219@gmail.com>
2022-06-22 15:09:43 +0900

    Also work on mac

--- sc/code.js
+++ sc/code.js
@@ -1,34 +1,33 @@
 let argvs = process.argv;
-let fs = require('fs');
+let fs = require("fs");
+let path = require("path");
 const { exec } = require("child_process");
-if(argvs.length <= 2) {
-    console.log("Usage: node code.js <number>");
-    process.exit(1);
+if (argvs.length <= 2) {
+  console.log("Usage: node code.js <number>");
+  process.exit(1);
 }
 
-console.log("Base Dir :" , process.cwd());
+console.log("Base Dir :", process.cwd());
 let baseDir = process.cwd();
 
 let fileName = argvs.slice(2).join("");
-console.log("File Name :" , fileName , ". cpp");
+console.log("File Name :", fileName, ". cpp");
 
 let fileFolder = fileName.slice(0, fileName.length - 2) + "__";
-console.log("File Folder :" , fileFolder);
+console.log("File Folder :", fileFolder);
 
 // createFolder
-let folderPath = baseDir + "\\" + fileFolder;
-console.log("Folder Path :" , folderPath);
+let folderPath = path.join(baseDir, fileFolder);
+console.log("Folder Path :", folderPath);
 
-if(!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
+if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
 
 // createFile
-let filePath = folderPath + "\\" + fileName + ".cpp";
-console.log("File Path :" , filePath);
+let filePath = path.join(folderPath, fileName + ".cpp");
+console.log("File Path :", filePath);
 
-let preset = fs.readFileSync(__dirname + "\\preset", "utf8");
+let preset = fs.readFileSync(path.join(__dirname, "preset"), "utf8");
 
-if(!fs.existsSync(filePath)) fs.writeFileSync(filePath, preset);
+if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, preset);
 
-exec("code " + filePath , (err, stdout, stderr) => {
-    
-})
+exec("code " + filePath, (err, stdout, stderr) => {});
