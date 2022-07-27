7ef97e6859a70dcf251c7f471c68e3a48f926a31
Oein <oein0219@gmail.com>
2022-07-27 17:18:13 +0900

    Add2543

---
+++ 25__/2543.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <numbers>
+#include <cassert>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int , int> pii;
+
+int dx[] = {0 , 0 , 1 , -1};
+int dy[] = {1 , -1 , 0 , 0};
+
+int main(){
+    cin.tie(0); cout.tie(0);
+    
+}
