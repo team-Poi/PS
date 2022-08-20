1346f6836d447932e3ad1f1d088c5e915d7770f2
Oein <oein0219@gmail.com>
2022-08-20 11:32:58 +0900

    AddA1008

---
+++ A10__/A1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout.precision(33);
+    int A;
+    int B;
+    cin >> A;
+    cin >> B;
+    cout << (double)A / B;
+    return 0;
+}
