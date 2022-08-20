d05b14fddd5fe2c8769a938c870d26938815d39f
Oein <oein0219@gmail.com>
2022-08-20 11:39:52 +0900

    AddA1846

--- A18__/A1846.cpp
+++ A18__/A1846.cpp
@@ -4,21 +4,49 @@
 #include <iostream>
 #include <map>
 #include <set>
-#include <math.h>
-#include <numeric>
-#include <numbers>
-#include <cassert>
+#include <cstring>
+#include <climits>
 
 using namespace std;
 
 typedef long long ll;
-typedef pair<int , int> pii;
+typedef pair<int, int> pi_i;
+typedef pair<pair<int, int>, pair<int, int>> ppi_i__pi_i;
+typedef pair<long long, long long> pll_ll;
 typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef map<int, int> mii;
+typedef map<string, int> ms_i;
+typedef map<string, string> ms_s;
+typedef set<int> si;
+typedef set<string> ss;
+typedef set<long long> sll;
 
-int dx[] = {0 , 0 , 1 , -1};
-int dy[] = {1 , -1 , 0 , 0};
+int n;
 
-int main(){
-    cin.tie(0); cout.tie(0);
-    
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    if (n == 3)
+    {
+        cout << "-1";
+        return 0;
+    }
+    int secSL = n / 2 + (n % 2);
+    int secSR = n / 2;
+
+    cout << secSL << "\n";
+    for (int i = 1; i < secSL; i++)
+    {
+        cout << i << "\n";
+    }
+    cout << n << "\n";
+    for (int i = 1; i < secSR; i++)
+    {
+        cout << i + secSL << "\n";
+    }
 }
