97bbef289116b0894f537c7a2d9b443482ec41a2
Oein <oein0219@gmail.com>
2022-09-07 17:22:59 +0900

    Add3428

--- 34__/3428.cpp
+++ 34__/3428.cpp
@@ -2,28 +2,33 @@
 using namespace std;
 vector<int> adj[2][100001];
 bool visited[100001];
-int N,M,X,k;
+int n, M, X, k;
 
-int dfs(int n){
-    visited[n]=1;
-    int ret=1;
-    for(auto v : adj[k][n]){
-        if(!visited[v]) ret+=dfs(v);
+int dfs(int n)
+{
+    visited[n] = 1;
+    int ret = 1;
+    for (auto v : adj[k][n])
+    {
+        if (!visited[v])
+            ret += dfs(v);
     }
     return ret;
 }
 
-int main(){
-    cin >> N >> M >> X;
-    for(int i=0;i<M;i++){
-        int u,v;
+int main()
+{
+    cin >> n >> M >> X;
+    for (int i = 0; i < M; i++)
+    {
+        int u, v;
         cin >> u >> v;
         adj[0][u].push_back(v);
         adj[1][v].push_back(u);
     }
-    int U,V;
-    U=N-dfs(X)+1;
-    k=1;
-    V=dfs(X);
+    int U, V;
+    U = n - dfs(X) + 1;
+    k = 1;
+    V = dfs(X);
     cout << V << " " << U;
 }
