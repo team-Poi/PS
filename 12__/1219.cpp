49cc3726b20ebf984518c48fb17d437b3f523520
Oein <oein0219@gmail.com>
2022-10-30 21:09:37 +0900

    idk

--- 12__/1219.cpp
+++ 12__/1219.cpp
@@ -3,55 +3,70 @@
 using namespace std;
 
 typedef unsigned long long ll;
-typedef pair<int , int> pii;
+typedef pair<int, int> pii;
 
 // 행 : 세로
 // 열 : 가로
 
-int boardHang , boardYul , countOfColorPaper , mistakePointCount , maxHang = 0;
-vector<int> mistakes;
-bool inputed[1000005] = {0,};
+int a, b, c, d, e = 0;
+vector<int> f;
+bool g[1000005] = {
+    0,
+};
 
-bool able(int wd){
+bool able(int wd)
+{
     int usedColorPaper = 0;
     int lastCover = -1;
-    for(auto now : mistakes){
-        if(usedColorPaper > countOfColorPaper) return false;
-        if(now <= lastCover) continue;
+    for (auto now : f)
+    {
+        if (usedColorPaper > c)
+            return false;
+        if (now <= lastCover)
+            continue;
 
         lastCover = now + wd - 1;
         usedColorPaper++;
     }
 
-    if(usedColorPaper > countOfColorPaper) return false;
+    if (usedColorPaper > c)
+        return false;
 
     return true;
 }
 
-int main(){
-    cin.tie(0); cout.tie(0);
-    cin >> boardHang >> boardYul >> countOfColorPaper >> mistakePointCount;
-    for(int i = 0;i <mistakePointCount;i++){
-        int hang , yul; // y , x
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    cin >> a >> b >> c >> d;
+    for (int i = 0; i < d; i++)
+    {
+        int hang, yul; // y , x
         cin >> hang >> yul;
-        maxHang = max(maxHang , hang);
-        if(!inputed[yul]){
-            mistakes.push_back(yul);
-            inputed[yul] = true;
+        e = max(e, hang);
+        if (!g[yul])
+        {
+            f.push_back(yul);
+            g[yul] = true;
         }
     }
 
-    sort(mistakes.begin() , mistakes.end());
+    sort(f.begin(), f.end());
 
-    int low = maxHang;
+    int low = e;
     int high = 1000000;
     int md;
-    while(low <= high){
-        int mid = (low +high) / 2;
-        if(able(mid)){
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (able(mid))
+        {
             high = mid - 1;
-            md=  mid;
-        }else{
+            md = mid;
+        }
+        else
+        {
             low = mid + 1;
         }
     }
