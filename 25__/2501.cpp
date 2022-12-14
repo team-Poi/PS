bebeae39a3ec3029ff4d9279a3b438beb5830efb
Oein <oein0219@gmail.com>
2022-12-14 18:22:29 +0900

    Add2501

---
+++ 25__/2501.cpp
@@ -0,0 +1,189 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+using namespace std;
+#define interface struct
+#define zx3f 1061109567
+#define itn int
+#define _DEBUG_ 0
+#define dout     \
+    if (_DEBUG_) \
+    cout
+typedef long long ll;
+typedef unsigned long ull;
+typedef pair<int, int> pii;
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+template <typename T>
+class CVector
+{
+public:
+    vector<T> vec;
+
+    CVector()
+    {
+    }
+    CVector(int size)
+    {
+        vec.reserve(size);
+    }
+    CVector(size_t size)
+    {
+        vec.reserve(size);
+    }
+
+    void push(T v)
+    {
+        vec.push_back(v);
+    }
+
+    void push_back(T v)
+    {
+        vec.push_back(v);
+    }
+
+    void remove_duplicate()
+    {
+        vec.erase(unique(vec.begin(), vec.end()), vec.end());
+    }
+
+    void fast_pop()
+    {
+        vec.pop_back();
+    }
+
+    void clear()
+    {
+        vec.clear();
+    }
+
+    T &at(size_t idx)
+    {
+        if (idx < 0)
+        {
+            idx = vec.size() + idx;
+        }
+        return this->vec[idx];
+    }
+
+    T pop()
+    {
+        T ld = vec.back();
+        vec.pop_back();
+        return ld;
+    }
+
+    T pop_back()
+    {
+        T ld = vec.back();
+        vec.pop_back();
+        return ld;
+    }
+
+    T back()
+    {
+        return vec.back();
+    }
+
+    T front()
+    {
+        return vec.front();
+    }
+
+    T &operator[](size_t idx)
+    {
+        return this->at(idx);
+    }
+
+    size_t size()
+    {
+        return vec.size();
+    }
+
+    size_t lastIndex()
+    {
+        return this->size() - 1;
+    }
+
+    bool empty()
+    {
+        return vec.empty();
+    }
+
+    auto begin()
+    {
+        return vec.begin();
+    }
+
+    auto end()
+    {
+        return vec.end();
+    }
+
+    auto capacity()
+    {
+        return vec.capacity();
+    }
+};
+
+CVector<int> shapes;
+
+int n, cnt[4] = {
+           0,
+},
+       mapping[3] = {1, 2, 3};
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        shapes.push(x);
+        cnt[x]++;
+    }
+
+    int ans = 1234567890;
+
+    while (true)
+    {
+        int tcnt[4][4] = {
+            0,
+        };
+
+        int a = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < cnt[mapping[i]]; j++)
+            {
+                tcnt[mapping[i]][shapes[a + j]]++;
+            }
+            a += cnt[mapping[i]];
+        }
+
+        ans = min(ans, tcnt[1][2] + tcnt[1][3] + max(tcnt[2][3], tcnt[3][2]));
+
+        // 3,2,1이면 break
+        // or 1,2,3 -> 1,3,2로 변경
+        if (!next_permutation(mapping, mapping + 3))
+            break;
+    }
+
+    cout << ans;
+}
