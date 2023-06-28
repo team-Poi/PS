a162a90747b7d9c6fff95e711536db263f73a7dd
SungHyun <62917247+Oein@users.noreply.github.com>
2023-06-28 17:14:30 +0900

    Create 9004.cpp

---
+++ 90__/9004.cpp
@@ -0,0 +1,31 @@
+// main.cpp
+#include <future>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <chrono>
+#include <vector>
+#include <sys/resource.h>
+#include <sys/time.h>
+// solution.cpp
+#include <iostream>
+// config defines
+#define PROBLEM 9004
+#define TIMEOUT 1000LL
+#define SOLUTION_FUNCTION main_
+
+using namespace std;
+
+int main_() {
+    cout << "   @@\n  @  @\n @    @\n@      @\n @    @\n  @  @\n   @@";
+    return 0;
+}
+
+#ifdef ONLINE_JUDGE
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    main_();
+    return 0;
+}
+#endif
