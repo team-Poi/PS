81b49f80232c6b440d64f0e8a081b2c387d062ba
SungHyun <62917247+Oein@users.noreply.github.com>
2023-08-09 15:28:06 +0900

    Prettier code

--- 10__/1014.cpp
+++ 10__/1014.cpp
@@ -1,87 +1,55 @@
-#include <vector>
-#include <algorithm>
-#include <queue>
 #include <iostream>
-#include <map>
+#include <string>
 #include <set>
-#include <math.h>
-#include <numeric>
+#include <map>
+#include <algorithm>
 #include <cassert>
-#include <stack>
-#include <cstring>
+#include <any>
+#include <thread>
 
 using namespace std;
 
-#define interface struct
-#define zx3f 1061109567
-
-typedef long long ll;
-typedef unsigned long ull;
-typedef pair<int, int> pii;
-
-typedef signed char i8;
-typedef short i16;
-typedef int i32;
-typedef long long i64;
-
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-
-string durumari;
-string bridge[3];
+string roll;
+string bridge[2];
 
 int dp[3][105][25];
 
-int main()
-{
-    cin.tie(0);
-    cout.tie(0);
-    cin >> durumari >> bridge[0] >> bridge[1];
-
-    int bs = bridge[0].size();
-    int ds = durumari.size();
-
-    for (int i = 0; i < bs; i++)
-    {
-        if (bridge[0][i] == durumari[0])
-            dp[0][i][0] = 1;
-        if (bridge[1][i] == durumari[0])
-            dp[1][i][0] = 1;
-    }
-
-    for (int i = 0; i < bs; i++)
-    {
-        for (int j = 1; j < ds; j++)
+void processIJ(int type, int i, int j) {
+    int moded = (type + 1) % 2;
+    if(bridge[type][i] == roll[j]) {
+        int cnt = 0;
+        
+        for (int k = 0; k < i; k++)
         {
-            if (bridge[0][i] == durumari[j])
-            {
-                int cnt = 0;
-                for (int k = 0; k < i; k++)
-                {
-                    if (dp[1][k][j - 1] != 0)
-                        cnt += dp[1][k][j - 1];
-                    dp[0][i][j] = cnt;
-                }
-            }
-
-            if (bridge[1][i] == durumari[j])
-            {
-                int cnt = 0;
-                for (int k = 0; k < i; k++)
-                {
-                    if (dp[0][k][j - 1] != 0)
-                        cnt += dp[0][k][j - 1];
-                    dp[1][i][j] = cnt;
-                }
-            }
+            cnt += dp[moded][k][j - 1];
+            dp[type][i][j] = cnt;
         }
     }
+}
 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> roll >> bridge[0] >> bridge[1];
+    
+    for(int i = 0;i < bridge[0].size();i++) {
+        if (bridge[0][i] == roll[0])
+            dp[0][i][0] = 1;
+        if (bridge[1][i] == roll[0])
+            dp[1][i][0] = 1;
+    }
+    
+    for(int i = 0;i < bridge[0].size();i++) {
+        for(int j = 1;j < roll.size(); j++) {
+            processIJ(0, i, j);
+            processIJ(1, i, j);
+        }
+    }
+    
     int ans = 0;
-    for (int i = 0; i < bs; i++)
+    for (int i = 0; i < bridge[0].size(); i++)
     {
-        ans += dp[0][i][ds - 1];
-        ans += dp[1][i][ds - 1];
+        ans += dp[0][i][roll.size() - 1];
+        ans += dp[1][i][roll.size() - 1];
     }
 
     cout << ans;
